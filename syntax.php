be17be9a5a38ca106c005cddeb79978ecf8e7cc7
scottleechua <scottleechua@gmail.com>
2025-08-11 12:24:42 +0800

    Remove deprecated require_once

--- syntax.php
+++ syntax.php
@@ -6,9 +6,6 @@
  */
 
 if(!defined('DOKU_INC')) die();
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-
-require_once DOKU_PLUGIN.'syntax.php';
 
 class syntax_plugin_footerv2 extends DokuWiki_Syntax_Plugin {
 
